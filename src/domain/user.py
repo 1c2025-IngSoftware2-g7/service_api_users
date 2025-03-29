29d4b0724d32e9046fbf259e82d6f5c164239858
Martina La Rosa <larosamf@gmail.com>
2025-03-28 22:44:53 -0300

    refactor: add domain layer

---
+++ src/domain/user.py
@@ -0,0 +1,9 @@
+""" The domain layer represents the underlying domain, mostly consisting of domain entities and, 
+in some cases, services. 
+- Business rules, like invariants and algorithms, should all stay in this layer.
+"""
+
+class User:
+    def __init___():
+        return
+    
