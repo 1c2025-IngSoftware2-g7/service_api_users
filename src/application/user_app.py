0e6dde3a12fce45687e61c725de01c6b06c0354f
Martina La Rosa <larosamf@gmail.com>
2025-03-28 22:54:01 -0300

    refactor: add application layer and presentation (controller) layer

---
+++ src/application/user_app.py
@@ -0,0 +1,9 @@
+""" 
+The application layer contains all the logic that is required by the application to meet its functional requirements and, 
+at the same time, is not a part of the domain rules. In most systems that I've worked with, the application layer consisted 
+of services orchestrating the domain objects to fulfill a use case scenario.
+"""
+
+class UserApp:
+    def __init___():
+        return
---
+++ src/presentation/user_controller.py
@@ -0,0 +1,8 @@
+""" 
+The presentation layer contains all of the classes responsible for presenting the UI to the end-user 
+or sending the response back to the client (in case we’re operating deep in the back-end).
+"""
+
+class UserController:
+    def __init___():
+        return
