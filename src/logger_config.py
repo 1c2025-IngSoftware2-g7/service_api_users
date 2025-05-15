6379cc37f51c6da038dc25d18059bdb8d4445343
Martina La Rosa <larosamf@gmail.com>
2025-05-15 14:15:40 -0300

    fix: do not duplicate logs

--- src/logger_config.py
+++ src/logger_config.py
@@ -3,6 +3,7 @@ import sys
 import logging
 import structlog
 
+LOGGER_NAME = "api-users"
 
 def add_service_tag(logger, method_name, event_dict):
     """Add tag in each log."""
@@ -11,11 +12,16 @@ def add_service_tag(logger, method_name, event_dict):
     return event_dict
 
 def configure_logging():
-    logging.basicConfig(
-        format="%(message)s",
-        stream=sys.stdout,
-        level=logging.DEBUG,
-    )
+    logger = logging.getLogger(LOGGER_NAME)
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    if not logger.handlers:
+        # avoid duplication
+        handler = logging.StreamHandler(sys.stdout)
+        handler.setLevel(logging.INFO)
+        handler.setFormatter(logging.Formatter("%(message)s"))
+        logger.addHandler(handler)
 
     structlog.configure(
         processors=[
@@ -23,7 +29,6 @@ def configure_logging():
             structlog.processors.TimeStamper(fmt="iso"),
             add_service_tag,
             structlog.processors.JSONRenderer()
-
         ],
         context_class=dict,
         logger_factory=structlog.stdlib.LoggerFactory(),
@@ -32,8 +37,8 @@ def configure_logging():
     )
 
 def get_logger(name: str = None):
-    if name:
-        return structlog.get_logger(name)
-    return structlog.get_logger()
+    std_logger = logging.getLogger(name or LOGGER_NAME)
+    std_logger.propagate = False  # do not duplicate the root log
+    return structlog.get_logger(name or LOGGER_NAME)
 
 configure_logging()
