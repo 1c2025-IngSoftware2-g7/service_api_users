7424469bb8bd2379d7652f955b8966c138379151
Martina La Rosa <larosamf@gmail.com>
2025-06-26 18:28:34 -0300

    test: add integ tests

--- tests/integration/test_auth.py
+++ tests/integration/test_auth.py
@@ -6,3 +6,8 @@ def test_login_admin_wrong_credentials(client):
 def test_login_admin_missing_data(client):
     response = client.post("/users/admin/login", json={})
     assert response.status_code == 400
+
+def test_login_users_missing_data(client):
+    response = client.post("/users/login", json={})
+    assert response.status_code == 400
+    assert isinstance(response.json, dict)
